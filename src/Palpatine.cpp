6a078c6bb75f2e5239059769a11a60e6904df31a
batunpc <batuhnpc@gmail.com>
2022-09-21 16:39:49 -0400

    revert due to linker error

--- src/Palpatine.cpp
+++ src/Palpatine.cpp
@@ -101,9 +101,21 @@ void Palpatine::process_path(string input, string output, string name) {
 
 void Palpatine::generate_page_file(string output, string title,
                                    vector<string> paragraphs) {
+
   ofstream html(output);
-  HMTLPLUS::header(html, title);
-  html << R"(<body>
+  html << R"(<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>)"
+       << title << "</title>\n";
+  for (auto &ss : stylesheet)
+    html << R"(    <link rel="stylesheet" href=")" << ss << R"(">)"
+         << std::endl;
+  html << R"(</head>
+
+<body>
     <div>
         <h1 class="animate__animated animate__bounce">)"
        << title << R"(</h1>
@@ -121,8 +133,20 @@ void Palpatine::generate_index_file(string output, string title,
                                     vector<string> directories,
                                     vector<string> files) {
   ofstream html(output);
-  HMTLPLUS::header(html, title);
-  html << R"(<body>
+  html << R"(<!DOCTYPE html> 
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>)"
+       << title << "</title>\n";
+  for (auto &ss : stylesheet)
+    html << R"(    <link rel="stylesheet" href=")" << ss << R"(">)"
+         << std::endl;
+  html << R"(</head>
+
+<body>
     <div>
         <h1>)"
        << title << R"(</h1>
