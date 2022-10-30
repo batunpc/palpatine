dd27327fa955ced28003ece9b4180a4164d5f272
batunpc <batuhnpc@gmail.com>
2022-10-29 20:46:20 -0400

    generated clang-format

--- src/Palpatine.cpp
+++ src/Palpatine.cpp
@@ -11,71 +11,76 @@ namespace fs = std::filesystem;
 using std::vector, std::ifstream, std::ofstream;
 
 // Constructor
-Palpatine::Palpatine(const char *output, const char *input,
-                     const char *stylesheet)
+Palpatine::Palpatine(
+    const char* output, const char* input, const char* stylesheet)
     : input(input) {
-  // paths to the input and output directories
-  this->output = (fs::path(output) / "dist").string();
+    // paths to the input and output directories
+    this->output = (fs::path(output) / "dist").string();
 
-  this->stylesheets = std::vector{string("")};
-  if (stylesheet != nullptr) {
-    this->stylesheets.emplace_back(stylesheet);
-  }
-  // remove the output directory if it exists
-  fs::remove_all(this->output);
-  // create the output directory if it doesn't exist
-  fs::create_directory(this->output);
+    this->stylesheets = std::vector{string("")};
+    if (stylesheet != nullptr) {
+        this->stylesheets.emplace_back(stylesheet);
+    }
+    // remove the output directory if it exists
+    fs::remove_all(this->output);
+    // create the output directory if it doesn't exist
+    fs::create_directory(this->output);
 }
 
 void Palpatine::generate() { process_path(input, output, "index.html"); }
 
-void Palpatine::process_path(const string &input, const string &output,
-                             const string &name) {
-  if (fs::is_directory(input)) {
-    // Store directories and files in current folder
-    std::vector<string> directories, files;
-    for (const auto &entry : fs::directory_iterator(input)) {
-      fs::path input_path = fs::path(entry);
-      if (entry.is_directory()) {
-        auto output_path = fs::path(output) / input_path.filename();
-        // Create the directory
-        fs::create_directory(output_path);
-        // Create the index file for the directory
-        process_path(input_path.string(), output_path.string(), "index.html");
+void Palpatine::process_path(
+    const string& input, const string& output, const string& name) {
+    if (fs::is_directory(input)) {
+        // Store directories and files in current folder
+        std::vector<string> directories, files;
+        for (const auto& entry : fs::directory_iterator(input)) {
+            fs::path input_path = fs::path(entry);
+            if (entry.is_directory()) {
+                auto output_path = fs::path(output) / input_path.filename();
+                // Create the directory
+                fs::create_directory(output_path);
+                // Create the index file for the directory
+                process_path(
+                    input_path.string(), output_path.string(), "index.html");
 
-        directories.push_back(input_path.filename().string());
-      } else if (entry.is_regular_file() && (input_path.extension() == ".txt" ||
-                                             input_path.extension() == ".md")) {
-        // Create the page file
-        process_path(input_path.string(), output,
-                     input_path.stem().string() + ".html");
+                directories.push_back(input_path.filename().string());
+            } else if (
+                entry.is_regular_file() && (input_path.extension() == ".txt" ||
+                                            input_path.extension() == ".md")) {
+                // Create the page file
+                process_path(
+                    input_path.string(), output,
+                    input_path.stem().string() + ".html");
 
-        files.push_back(input_path.stem().string());
-      }
-      string title = fs::relative(input_path, this->input).string();
-      if (title == ".") {
-        title = "Homepage";
-      }
-      generate_index_file((fs::path(output) / name).string(), title,
-                          directories, files);
+                files.push_back(input_path.stem().string());
+            }
+            string title = fs::relative(input_path, this->input).string();
+            if (title == ".") {
+                title = "Homepage";
+            }
+            generate_index_file(
+                (fs::path(output) / name).string(), title, directories, files);
+        }
+    } else if (
+        fs::path(input).extension() == ".txt" ||
+        fs::path(input).extension() == ".md") {
+        Handler* handler = fs::path(input).extension() == ".txt"
+                               ? new TextHandler(stylesheets)
+                               : new MarkdownHandler(stylesheets);
+        handler->process(input, output, name);
+        delete handler;
+    } else {
+        utils_sdds::print_error(
+            "Error: " + input + " is not a valid file type");
+        std::terminate();
     }
-  } else if (fs::path(input).extension() == ".txt" ||
-             fs::path(input).extension() == ".md") {
-    Handler *handler = fs::path(input).extension() == ".txt"
-                           ? new TextHandler(stylesheets)
-                           : new MarkdownHandler(stylesheets);
-    handler->process(input, output, name);
-    delete handler;
-  } else {
-    utils_sdds::print_error("Error: " + input + " is not a valid file type");
-    std::terminate();
-  }
 }
 
-void Palpatine::generate_index_file(string output, const string &title,
-                                    const vector<string> &directories,
-                                    const vector<string> &files) {
-  ofstream html(output);
-  HMTLPLUS::header(html, title, this->stylesheets);
-  HMTLPLUS::index_body(html, title, directories, files);
+void Palpatine::generate_index_file(
+    string output, const string& title, const vector<string>& directories,
+    const vector<string>& files) {
+    ofstream html(output);
+    HMTLPLUS::header(html, title, this->stylesheets);
+    HMTLPLUS::index_body(html, title, directories, files);
 }
--- src/Palpatine.h
+++ src/Palpatine.h
@@ -7,17 +7,17 @@ using std::string, std::vector;
 
 class Palpatine {
 public:
-  // Palpatine(string, string, string);
-  Palpatine();
-  Palpatine(const char *, const char *, const char *);
-  void generate();
-  auto get_stylesheet() -> vector<string> { return stylesheets; }
+    // Palpatine(string, string, string);
+    Palpatine();
+    Palpatine(const char*, const char*, const char*);
+    void generate();
+    auto get_stylesheet() -> vector<string> { return stylesheets; }
 
 private:
-  string input, output;
-  std::vector<string> stylesheets;
-  void process_path(const string &, const string &, const string &);
+    string              input, output;
+    std::vector<string> stylesheets;
+    void process_path(const string&, const string&, const string&);
 
-  void generate_index_file(string, const string &, const vector<string> &,
-                           const vector<string> &);
+    void generate_index_file(
+        string, const string&, const vector<string>&, const vector<string>&);
 };
