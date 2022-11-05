8929d027341c085259e151e005fc78dc0a6a1f76
batunpc <batuhnpc@gmail.com>
2022-11-04 22:19:27 -0400

    static analyzer ⚙ module update

--- cmake/modules/StaticAnalyzers.cmake
+++ cmake/modules/StaticAnalyzers.cmake
@@ -15,14 +15,12 @@ if(ENABLE_CLANG_TIDY)
         set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_COMMAND};-p=${CMAKE_BINARY_DIR};${CLANGTIDY_EXTRA_ARGS}" CACHE STRING "" FORCE)
         set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-        # after the first run clang-tidy warnings disappear from the IDE 
-        # keep the warnings in the IDE by running clang-tidy again after the build
         add_custom_target(clang-tidy
             COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target ${CMAKE_PROJECT_NAME}
             COMMAND ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target clang-tidy
             COMMENT "Running clang-tidy..."
         )
-        # show clang-tidy warnings in the IDE vscode
+
         set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
         
     endif()
